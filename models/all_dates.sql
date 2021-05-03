93203a94bf30541c85cbf9acf327d763c1991928
coapacetic <kyle.d.coapman@gmail.com>
2021-05-03 09:34:38 -0400

    adapt all_dates.sql to work on bq

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -1,11 +1,11 @@
 {{ config (
-    materialized="table",
-    enabled=false
-)}}
+    materialized="table"
+)
+}}
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="to_date('01/01/2020', 'mm/dd/yyyy')",
-    end_date="to_date('01/01/2021', 'mm/dd/yyyy')"
+    start_date="cast('2020-01-01' as date)",
+    end_date="cast('2021-01-01' as date)"
    )
 }}
